869b5f9c264e845649684ae6ce758ff027addb93
smruthi lattupalli <smruthireddy@gmail.com>
2020-02-16 15:07:36 +0100

    written plateau specs and defined plateau object

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
 
+# Plateau class definition
+class Plateau
+  attr_reader :west_limit, :south_limit, :north_limit, :east_limit
+
+  def initialize(plateau_limits)
+    x_limit = plateau_limits[0].to_i
+    y_limit = plateau_limits[1].to_i
+
+    @west_limit = 0
+    @south_limit = 0
+    @east_limit = x_limit
+    @north_limit = y_limit
+  end
+end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -1,1 +1,23 @@
+# frozen_string_literal: true
 
+require_relative '../lib/plateau'
+
+describe 'Plateau :: Check Plateau Limits' do
+  plateau = Plateau.new(%w[4 4])
+
+  it 'Validate west limit' do
+    expect(plateau.west_limit).to eq(0)
+  end
+
+  it 'Validate south limit' do
+    expect(plateau.south_limit).to eq(0)
+  end
+
+  it 'Validate east limit' do
+    expect(plateau.east_limit).to eq(4)
+  end
+
+  it 'Validate north limit' do
+    expect(plateau.north_limit).to eq(4)
+  end
+end
